695be4862acea54e1ba809ddfbc285b4f70707b3
hanna0601 <zeyuzh.zhang@mail.utoronto.ca>
2024-05-27 23:05:02 -0400

    update3

--- Assets/Scripts/EVOGAMI/Movement/HumanLocomotion.cs
+++ Assets/Scripts/EVOGAMI/Movement/HumanLocomotion.cs
@@ -18,12 +18,18 @@ namespace EVOGAMI.Movement
         protected override void FixedUpdate()
         {
             base.FixedUpdate();
-            ManageAnimations();
+            ManageAnimations(Time.deltaTime);
         }
 
-        private void ManageAnimations()
+        private void ManageAnimations(float delta)
         {
-            animator.SetBool("isWalk", _inputManager.IsMoving);  // Directly set the 'isWalk' animation parameter
+            // animator.SetBool("isWalk", InputManager.IsMoving);  // Directly set the 'isWalk' animation parameter
+            float horizontal = InputManager.MoveInput.magnitude;
+            float vertical = PlayerManager.PlayerRb.velocity.y; 
+            
+            animator.SetFloat("vertical", vertical, 0.1f, delta);
+            animator.SetFloat("horizontal", horizontal, 0.1f, delta);
+            
         }
     }
 }
