77725babf61c8904a0103ba2b46cbe163565e6d6
Daria Illarionova <59188733+daria-illarionova@users.noreply.github.com>
2024-06-07 16:23:24 -0400

    Clean up human movement script. (#29)
    * Clean up human movement script.
    
    * Remove unused variables.
    
    ---------
    
    Co-authored-by: Lance Li <lanceli1416@gmail.com>

--- Assets/Scripts/Movement/HumanLocomotion.cs
+++ Assets/Scripts/Movement/HumanLocomotion.cs
@@ -17,10 +17,7 @@ namespace EVOGAMI.Movement
         
         [SerializeField] private float obstacleCheckLength = 0.3f;
         [SerializeField] private float obstacleCheckRadius = 0.2f;
-        [SerializeField] private float detectionRange = 0.1f;
-        // [SerializeField] private LayerMask obstacleLayer;
         private RaycastHit _obstacleHit;
-        // private bool _isTouchingObstacle;
            
         [Header("Vaulting")]
         [SerializeField] private float vaultSpeed = .75f;
@@ -30,9 +27,6 @@ namespace EVOGAMI.Movement
         {
             // Set the form
             form = OrigamiContainer.OrigamiForm.Human;
-            
-            // Bind input actions
-            // InputManager.Instance.Controls.Player.Jump.performed += ctx => OnJumpPerformed();
         }
             
         protected override void Start()
@@ -46,22 +40,11 @@ namespace EVOGAMI.Movement
         
         protected override void FixedUpdate()
         {
-            // base.FixedUpdate();
-            // ManageAnimations(Time.deltaTime);
-            
             if (!isVaulting)
             {
                 base.FixedUpdate();
-                // GroundCheck();
-                // DetectObstacles();
                 ManageAnimations(Time.deltaTime);
             }
-
-            // DetectObstacles();
-            // Debug.Log(isObstacleDetected);
-
-            // If human (hand) touching obstacle => ???
-            // if (_isTouchingObstacle)
         }
         
         #region Input Events
@@ -81,9 +64,6 @@ namespace EVOGAMI.Movement
         #endregion
         
         #region Vaulting
-        
-        // TODO: should param delta be included?
-        // <param name="delta">The time between frames</param>
 
         /// <summary>
         ///     Vault over obstacle.
@@ -91,8 +71,7 @@ namespace EVOGAMI.Movement
         /// <param name="obstacle">The time between frames</param>
         private void Vault(Collider obstacle)
         {
-            // Disable player control during vault.
-            //DisablePlayerControl();
+            // Disable player control during vault: DisablePlayerControl();
 
             // Calculate the target position for vaulting.
             Vector3 vaultTarget = obstacle.transform.position + new Vector3(0, obstacle.bounds.size.y + 0.5f, 0); 
@@ -117,42 +96,16 @@ namespace EVOGAMI.Movement
 
             PlayerTransform.position = targetPosition;
 
-            // Re-enable player control after vault.
-            // EnablePlayerControl();
+            // Re-enable player control after vault: EnablePlayerControl();
             
             // Reset vault animation here
             // animator.ResetTrigger("Vault");
         }
         
-        /*
-        private void DisablePlayerControl()
-        {
-            isVaulting = true;
-            
-            // Disable player input.
-            // InputManager.Instance.Controls.Disable();
-            
-            // Disable player movement input.
-            InputManager.Instance.Controls.Player.Disable();
-        }
-
-        private void EnablePlayerControl()
-        {
-            isVaulting = false;
-            
-            // Re-enable player input.
-            // InputManager.Instance.Controls.Enable();
-            
-            // Re-enable player movement input.
-            InputManager.Instance.Controls.Player.Enable();
-        }
-        */
         #endregion
         
         #region Collision
 
-        private bool isObstacleDetected = false;
-        
         /// <summary>
         ///     Check if the player is in front of a vaultable obstacle.
         /// </summary>
@@ -160,14 +113,7 @@ namespace EVOGAMI.Movement
         {
             LayerMask obstacleLayer = LayerMask.GetMask("Ground", "Default", "Wall");
             if (Physics.SphereCast(obstacleCheck.position, obstacleCheckRadius, obstacleCheck.forward, out _obstacleHit, obstacleCheckLength))
-            // if (
-            //     Physics.SphereCast(obstacleCheck.position, obstacleCheckRadius, obstacleCheck.forward, out _obstacleHit, detectionRange, obstacleLayer) ||
-            //     Physics.SphereCast(obstacleCheck.position, obstacleCheckRadius, -obstacleCheck.forward, out _obstacleHit, detectionRange, obstacleLayer) ||
-            //     Physics.SphereCast(obstacleCheck.position, obstacleCheckRadius, -obstacleCheck.right, out _obstacleHit, detectionRange, obstacleLayer) ||
-            //     Physics.SphereCast(obstacleCheck.position, obstacleCheckRadius, obstacleCheck.right, out _obstacleHit, detectionRange, obstacleLayer)
-            //     )
             {
-                // TODO: create "obstacle" tag 
                 if (_obstacleHit.collider.CompareTag("obstacle"))
                 {
                     // float obstacleHeight = _obstacleHit.collider.bounds.size.y;
@@ -177,12 +123,10 @@ namespace EVOGAMI.Movement
                     // {
                         // Debug.Log("Obstacle detected and suitable for vaulting.");
                         Vault(_obstacleHit.collider);
-                        isObstacleDetected = true;
                         return true;
                     // }
                 }
             }
-            isObstacleDetected = false;
             return false;
         }
         
@@ -216,61 +160,28 @@ namespace EVOGAMI.Movement
     }
 }
 
-        /*
-        [SerializeField] private Animator animate;  // Ensure this is assigned in the Unity Editor
-
-        protected override void Start()
+/*
+ ARCHIVED METHODS 2 Disable/Enable player control
+ 
+        private void DisablePlayerControl()
         {
-            base.Start();
-            if (!animator)
-            {
-                animator = GetComponent<Animator>();
-            }
+            isVaulting = true;
+
+            // Disable player input.
+            // InputManager.Instance.Controls.Disable();
+
+            // Disable player movement input.
+            InputManager.Instance.Controls.Player.Disable();
         }
 
-        protected override void FixedUpdate()
+        private void EnablePlayerControl()
         {
-            base.FixedUpdate();
-            ManageAnimations(Time.deltaTime);
+            isVaulting = false;
+
+            // Re-enable player input.
+            // InputManager.Instance.Controls.Enable();
+
+            // Re-enable player movement input.
+            InputManager.Instance.Controls.Player.Enable();
         }
-
-        private void ManageAnimations(float delta)
-        {
-            // animator.SetBool("isWalk", InputManager.IsMoving);  // Directly set the 'isWalk' animation parameter
-            float horizontal = InputManager.MoveInput.magnitude;
-            float vertical = PlayerManager.PlayerRb.velocity.y;
-
-            animator.SetFloat("vertical", vertical, 0.1f, delta);
-            animator.SetFloat("horizontal", horizontal, 0.1f, delta);
-
-        }
-        */
-        
-        /*
-        #region Input Events
-           protected override void OnJumpPerformed()
-           {
-               // Check if an obstacle is in front and vault if detected
-               if (Physics.Raycast(obstacleCheck.position, obstacleCheck.forward, out _obstacleHit, detectionRange))
-               {
-                   if (_obstacleHit.collider.CompareTag("obstacle"))
-                   {
-                       float obstacleHeight = _obstacleHit.collider.bounds.size.y;
-                       float playerHeight = GetComponent<CapsuleCollider>().height;
-
-                       if (obstacleHeight <= (1.0f / 3.0f) * playerHeight)
-                       {
-                           Vault(_obstacleHit.collider);
-                           return;
-                       }
-                   }
-               }
-
-               // If no obstacle is detected, perform the regular jump
-               if (IsGrounded)
-               {
-                   PlayerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-               }
-           }
-           #endregion
-        */
+*/
