d7148774dd8dd356bc35695a434d23d92034ce2f
Lance Li <lanceli1416@gmail.com>
2024-06-14 04:27:04 -0400

    Organize art assets

--- Assets/Editor/TagSelectorPropertyDrawer.cs
+++ Assets/Editor/TagSelectorPropertyDrawer.cs
@@ -1,7 +1,27 @@
+using EVOGAMI.Custom.Attributes;
+using UnityEditor;
+using UnityEngine;
+
 namespace EVOGAMI.Editor
 {
-    public class TagSelectorPropertyDrawer
+    [CustomPropertyDrawer(typeof(TagSelectorAttribute))]
+    public class TagSelectorPropertyDrawer : PropertyDrawer
     {
-        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType == SerializedPropertyType.String)
+            {
+                EditorGUI.BeginProperty(position, label, property);
+
+                // Use the default tag selector
+                property.stringValue = EditorGUI.TagField(position, label, property.stringValue);
+
+                EditorGUI.EndProperty();
+            }
+            else
+            {
+                EditorGUI.PropertyField(position, property, label);
+            }
+        }
     }
 }
