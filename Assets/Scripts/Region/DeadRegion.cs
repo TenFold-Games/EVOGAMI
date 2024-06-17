67647a03ec4c358d83a2ae6b7aa53de936a8228b
Lance Li <lanceli1416@gmail.com>
2024-06-17 15:16:37 -0400

    Add death sfx

--- Assets/Scripts/Region/DeadRegion.cs
+++ Assets/Scripts/Region/DeadRegion.cs
@@ -1,17 +1,31 @@
+using EVOGAMI.Audio;
 using EVOGAMI.Core;
 using UnityEngine;
 
 namespace EVOGAMI.Region
 {
-    public class DeadRegion : CallbackRegion
+    public class DeadRegion : 
+        CallbackRegion,
+        ISfxPlayer
     {
+        [Header("Audio")]
+        // The audio source for the dead region
+        [SerializeField] [Tooltip("The audio source for the dead region")]
+        private AudioSource audioSource;
+        
         protected override void OnTriggerEnter(Collider other)
         {
             if (!IsConditionMet(other)) return;
 
+            PlaySfx(audioSource);
             onRegionEnter.Invoke(other);
             
             PlayerManager.Instance.RespawnAtLastCheckpoint();
+        }
+
+        public void PlaySfx(AudioSource sfx)
+        {
+            sfx.Play();
         }
     }
 }
