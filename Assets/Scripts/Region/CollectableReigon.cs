c48d4ee56a952db0b2a8ac065d4a004f344c44b9
Lance Li <lanceli1416@gmail.com>
2024-06-17 14:54:14 -0400

    Add crane collected sfx

--- Assets/Scripts/Region/CollectableReigon.cs
+++ Assets/Scripts/Region/CollectableReigon.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using EVOGAMI.Core;
 using UnityEngine;
 
@@ -5,13 +6,40 @@ namespace EVOGAMI.Region
 {
     public class CollectableRegion : CallbackRegion
     {
+        [Header("Display")]
+        // The MeshRenderer for the collectable region
+        [SerializeField] [Tooltip("The MeshRenderer for the collectable region")]
+        private MeshRenderer meshRenderer;
+        // The Collider for the collectable region
+        [SerializeField] [Tooltip("The Collider for the collectable region")]
+        private Collider m_collider;
+        
+        [Header("Audio")]
+        // The audio source for the collectable region
+        [SerializeField] [Tooltip("The audio source for the collectable region")]
+        private AudioSource audioSource;
         protected override void OnTriggerEnter(Collider other)
         {
             if (!IsConditionMet(other)) return;
             
             onRegionEnter.Invoke(other);
+            Disappear();
             
             PlayerManager.Instance.CraneCollected();
+            StartCoroutine(PlayAudioAndDestroy());
+        }
+        
+        private void Disappear()
+        {
+            meshRenderer.enabled = false;
+            m_collider.enabled = false;
+        }
+        
+        private IEnumerator PlayAudioAndDestroy()
+        {
+            audioSource.Play();
+            yield return new WaitForSeconds(audioSource.clip.length);
+            audioSource.Stop();
             Destroy(gameObject);
         }
     }
