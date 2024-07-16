b0d0757e8007b10a366be456fad73e94d153e160
hanna0601 <zeyuzh_zhang@hotmail.com>
2024-07-15 20:56:08 -0400

    Add gain form effect

--- Assets/Scripts/Region/GainFormRegion.cs
+++ Assets/Scripts/Region/GainFormRegion.cs
@@ -1,11 +1,60 @@
 ﻿using EVOGAMI.Core;
 using EVOGAMI.Origami;
 using UnityEngine;
+using EVOGAMI.Utils;
+using EVOGAMI.Audio;
 
 namespace EVOGAMI.Region
 {
-    public class GainFormRegion : CallbackRegion
+    public class GainFormRegion : 
+        CallbackRegion,
+        IAudioPlayer
     {
+        
+        [Header("Display")]
+        // The MeshRenderer for the collectable region
+        [SerializeField] [Tooltip("The MeshRenderer for the collectable region")]
+        private MeshRenderer meshRenderer;
+        // The Collider for the collectable region
+        [SerializeField] [Tooltip("The Collider for the collectable region")]
+        private Collider m_collider;
+        
+        [SerializeField] private float rotationSpeed = 10f;
+        
+        [Header("Audio")]
+        // The audio source for the collectable region
+        [SerializeField] [Tooltip("The audio source for the collectable region")]
+        private AudioSource audioSource;
+        public void PlayAudio(AudioSource sfx)
+        {
+            sfx.Play();
+        }
+        
+        private void PlayAudioAndDestroy()
+        {
+            PlayAudio(audioSource);
+            StartCoroutine(CoroutineUtils.DelayAction(audioSource.clip.length, () =>
+            {
+                audioSource.Stop();
+                Destroy(gameObject);
+            }));
+        }
+        
+        private void Update()
+        {
+            // Rotate the object slowly around the Y-axis
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+        
+        private void Disappear()
+        {
+            meshRenderer.enabled = false;
+            m_collider.enabled = false;
+        }
+
+
+
+    
         [Header("Settings")]
         // The form to gain when the player enters this region
         [SerializeField] [Tooltip("The form to gain when the player enters this region")]
@@ -16,8 +65,12 @@ namespace EVOGAMI.Region
             if (!IsConditionMet(other)) return;
             
             onRegionEnter.Invoke(other);
+            Disappear();
 
             PlayerManager.Instance.GainForm(form);
+            PlayAudioAndDestroy();
+
         }
     }
+    
 }
