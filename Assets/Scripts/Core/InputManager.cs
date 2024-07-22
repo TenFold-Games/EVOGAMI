4fa4df18492db74c8370971840b303371a34010c
hanna0601 <zeyuzh_zhang@hotmail.com>
2024-07-21 21:57:10 -0400

    Temporary remove cancel

--- Assets/Scripts/Core/InputManager.cs
+++ Assets/Scripts/Core/InputManager.cs
@@ -156,10 +156,10 @@ namespace EVOGAMI.Core
             Controls.UI.Pause.performed += PausePerformedCallback;
             Controls.UI.Pause.canceled += PauseCancelledCallback;
             
-            // UI - Cancel
-            Controls.UI.Cancel.started += PauseStartedCallback;
-            Controls.UI.Cancel.performed += PausePerformedCallback;
-            Controls.UI.Cancel.canceled += PauseCancelledCallback;
+            // // UI - Cancel
+            // Controls.UI.Cancel.started += PauseStartedCallback;
+            // Controls.UI.Cancel.performed += PausePerformedCallback;
+            // Controls.UI.Cancel.canceled += PauseCancelledCallback;
             
             // Origami - Transform
             Controls.Origami.Transform.started += TransformStartedCallback;
